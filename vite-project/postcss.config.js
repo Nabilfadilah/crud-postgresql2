33447bc0a08d84216017a76119121e51b318af95
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-04 20:14:27 +0700

    Install vite vue pada projek laravel

---
+++ vite-project/postcss.config.js
@@ -0,0 +1,14 @@
+// export default {
+//     plugins: {
+//       "@tailwindcss/postcss": {}, // Menggunakan package baru
+//       autoprefixer: {},
+//     },
+//   };
+  
+export default {
+    plugins: {
+      tailwindcss: {}, // ✅ Gunakan ini
+      autoprefixer: {},
+    },
+};
+  
